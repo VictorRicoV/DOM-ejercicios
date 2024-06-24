d4ef8eb026c350a347f9c0bd10372f6dc3e55282
Victor <lessricc@gmail.com>
2024-06-24 16:45:19 +0200

    terminados

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import "../scss/styles.scss";
+import { list } from 'postcss';
+import '../scss/styles.scss';
 
 //- Partiendo de este HTML selecciona en JavaScript el h3 y utilizándolo como punto de partida imprime la información de todos los elementos que hay en formato "familia": Soy un h3 con el id... y la clase... mi padre es un header con.... mi hermano es un h1 con....
 
-const starPointElement = document.getElementById("start-point");
+const starPointElement = document.getElementById('start-point');
 
 console.dir(starPointElement);
 console.log(
@@ -12,9 +13,31 @@ console.log(
 
 //- Partiendo de esta lista, intercambia los textos de los li, deberás tener este resultado sin modificar a mano el HTML. Importante, no hay que mover el <li> sólo cambiar el contenido.
 
-const exchangeText1 = document.getElementById("first-list");
-const exchangeText2 = document.getElementById("second-list");
+const exchangeText1 = document.getElementById('first-list');
+const exchangeText2 = document.getElementById('second-list');
 
-exchangeText1.textContent = "Primer li";
-exchangeText2.textContent = "Segundo li";
-console.dir(exchangeText1, exchangeText2);
+const firstListElementContent = exchangeText1.textContent;
+const secondListElementContent = exchangeText2.textContent;
+
+exchangeText1.textContent = secondListElementContent;
+exchangeText2.textContent = firstListElementContent;
+
+//- Usando este HTML muestra por consola el número de etiquetas que hay de cada cosa.
+// - Hay 1 Header
+// - Hay 1 h1
+// - Hay 2 nav
+//   ......
+
+const allHeader = document.querySelectorAll('header');
+const allNav = document.querySelectorAll('nav');
+const allUl = document.querySelectorAll('ul');
+const allLi = document.querySelectorAll('li');
+const allSection = document.querySelectorAll('section');
+const allArticle = document.querySelectorAll('article');
+const allTitles = document.querySelectorAll('h1, h2');
+const allP = document.querySelectorAll('p');
+const allA = document.querySelectorAll('a');
+const allImg = document.querySelectorAll('img');
+const allFooter = document.querySelectorAll('footer');
+
+console.log(allHeader, allNav, allUl, allLi, allSection, allArticle, allTitles, allP, allA, allImg, allFooter);
